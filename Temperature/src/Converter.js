f002fa94a338ce271008a7f61758a1401a523b77
User <vipanda569@mail.ru>
2019-07-08 22:05:31 +0700

    Temperature

---
+++ Temperature/src/Converter.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var convertButton = document.querySelector(".converter");
+
+ /*   var enterValue = document.getElementById("celsiusTemperature");
+    enterValue.value = " ";
+    function isEmpty(str) {
+        return (typeof str === "undefined" || str === null || str === "");
+    }
+    if (isEmpty(enterValue.value)) {
+        prompt("Вы не ввели ничего!");
+    } */
+        convertButton.addEventListener("click", function () {
+            var celsiusResult = document.getElementById("celsiusTemperature").value;
+            var fahrenheitResult = document.getElementById("fahrenheitConverter");
+            var kelvinResult = document.getElementById("kelvinConverter");
+            if (/[^[0-9 .]/.test(celsiusResult) || /^undefined$/.test(celsiusResult)) {
+                alert("Нужно вводить только цифры!");
+            } else {
+                kelvinResult.innerHTML = "Результат перевода по Кельвину: " + (celsiusResult * 1 + 273.15);
+                fahrenheitResult.innerHTML = "Результат перевода по Фаренгейту: " + (celsiusResult * 1.8 + 32);
+            }
+        });
+});
