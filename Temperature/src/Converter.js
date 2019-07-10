79acc6525bc4fc7d6f0e30eaf95ce1ac9060b669
User <vipanda569@mail.ru>
2019-07-10 15:20:01 +0700

    Temperature

--- Temperature/src/Converter.js
+++ Temperature/src/Converter.js
@@ -1,23 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     var convertButton = document.querySelector(".converter");
-    /*   var enterValue = document.getElementById("celsiusTemperature");
-       enterValue.value = " ";
-       function isEmpty(str) {
-           return (typeof str === "undefined" || str === null || str === "");
-       }
-       if (isEmpty(enterValue.value)) {
-           prompt("Вы не ввели ничего!");
-       } */
     convertButton.addEventListener("click", function () {
-        var celsiusResult = document.getElementById("celsiusTemperature").value;
+        var celsiusResult = Number(document.getElementById("celsiusTemperature").value);
         var fahrenheitResult = document.getElementById("fahrenheitConverter");
         var kelvinResult = document.getElementById("kelvinConverter");
 
-        if (/-[^[0-9 .]/.test(celsiusResult) && /^ - /.test(celsiusResult) || /^undefined$/.test(celsiusResult)) {
+        if (isNaN(celsiusResult)) {
             alert("Нужно вводить только цифры!");
         } else {
-            kelvinResult.innerHTML = "Результат перевода по Кельвину: " + (celsiusResult * 1 + 273.15);
-            fahrenheitResult.innerHTML = "Результат перевода по Фаренгейту: " + (celsiusResult * 1.8 + 32);
+            function getKelvin(celsiusResult) {
+                return "Результат перевода по Кельвину: " + (celsiusResult + 273.15);
+            }
+
+            kelvinResult.innerHTML = getKelvin(celsiusResult);
+
+            function getFahrenheit(celsiusResult) {
+                return "Результат перевода по Фаренгейту: " + (celsiusResult * 1.8 + 32);
+            }
+
+            fahrenheitResult.innerHTML = getFahrenheit(celsiusResult);
         }
     });
 });
