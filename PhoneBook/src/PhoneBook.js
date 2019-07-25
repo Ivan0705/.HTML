fc7373159dae9823f1d6005561b91ae64a2ebe9b
User <vipanda569@mail.ru>
2019-07-25 16:03:59 +0700

    PhoneBook

---
+++ PhoneBook/src/PhoneBook.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var table = $('#table1');
+    var tbody = $("#tbody1");
+    var $button = $('#add');
+    var newSurname = $("#new-surname");
+    var newName = $("#new-name");
+    var newNumberPhone = $("#new-number-phone");
+    var people = [];
+
+    function newTr() {
+        tbody.html('');
+        for (var i = 0; i < people.length; i++) {
+            var numberItem = i + 1;
+            table.append('<tr><td>' + numberItem + '</td><td>' + people[i].surname + '</td><td>' + people[i].name + '</td><td>' + people[i].numberPhone + '</td><td> <button  >X</button></td></tr>');
+        }
+    }
+
+    $button.click(function addPerson() {
+        var userSurname = newSurname.val();
+        var userName = newName.val();
+        var userNumberPhone = newNumberPhone.val();
+
+        if (userNumberPhone === '' && userSurname === '' && userName === '') {
+            alert("Все поля не заполнены!");
+            return;
+        } else if (userNumberPhone === '') {
+            alert("Не ввели номер телефона!");
+            return;
+        } else if (userName === '') {
+            alert("Не ввели имя!");
+            return;
+        } else if (userSurname === '') {
+            alert("Не ввели фамилию!");
+            return;
+        }
+        table.show();
+        var person = {
+            name: newName.val(),
+            surname: newSurname.val(),
+            numberPhone: newNumberPhone.val()
+        };
+        people.push(person);
+        newTr();
+        newName.val('');
+        newSurname.val('');
+        newNumberPhone.val('');
+    });
+    table.click(function deletePerson(event) {
+        var element = event.target.closest('tr');
+        var elementToIndex = table.find('tr').index(element);
+        people.splice(elementToIndex - 1, 1);
+        newTr();
+    });
+});
