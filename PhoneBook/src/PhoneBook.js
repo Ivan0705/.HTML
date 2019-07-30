4eebb69118739628524e374352024979e5e5d58c
User <vipanda569@mail.ru>
2019-07-30 14:49:30 +0700

    PhoneBook

--- PhoneBook/src/PhoneBook.js
+++ PhoneBook/src/PhoneBook.js
@@ -7,48 +7,52 @@ $(document).ready(function () {
     var newNumberPhone = $("#new-number-phone");
     var people = [];
 
-    function newTr() {
-        tbody.html('');
+    function buildTr() {
+        tbody.html("");
         for (var i = 0; i < people.length; i++) {
             var numberItem = i + 1;
-            table.append('<tr><td>' + numberItem + '</td><td>' + people[i].surname + '</td><td>' + people[i].name + '</td><td>' + people[i].numberPhone + '</td><td> <button  >X</button></td></tr>');
+            table.append("<tr><td>" + numberItem + "</td><td>" + people[i].surname + "</td><td>" + people[i].name + "</td><td>" + people[i].numberPhone + "</td><td> <button  >X</button></td></tr>");
         }
     }
 
-    $button.click(function addPerson() {
+    $button.click(function () {
         var userSurname = newSurname.val();
         var userName = newName.val();
         var userNumberPhone = newNumberPhone.val();
 
-        if (userNumberPhone === '' && userSurname === '' && userName === '') {
+        if (userNumberPhone === "" && userSurname === "" && userName === "") {
             alert("Все поля не заполнены!");
             return;
-        } else if (userNumberPhone === '') {
+        } else if (userNumberPhone === "") {
             alert("Не ввели номер телефона!");
             return;
-        } else if (userName === '') {
+        } else if (userName === "") {
             alert("Не ввели имя!");
             return;
-        } else if (userSurname === '') {
+        } else if (userSurname === "") {
             alert("Не ввели фамилию!");
             return;
         }
+
         table.show();
         var person = {
             name: newName.val(),
             surname: newSurname.val(),
             numberPhone: newNumberPhone.val()
         };
+
         people.push(person);
-        newTr();
-        newName.val('');
-        newSurname.val('');
-        newNumberPhone.val('');
+        buildTr();
+
+        newName.val("");
+        newSurname.val("");
+        newNumberPhone.val("");
     });
+
     table.click(function deletePerson(event) {
-        var element = event.target.closest('tr');
-        var elementToIndex = table.find('tr').index(element);
+        var element = event.target.closest("tr");
+        var elementToIndex = table.find("tr").index(element);
         people.splice(elementToIndex - 1, 1);
-        newTr();
+        buildTr();
     });
 });
