d48a1fb1f3c7f76ef87bb545ab9b81abaf96e4d3
User <vipanda569@mail.ru>
2019-08-31 19:36:32 +0700

    ExpressPhoneBook

---
+++ myapp/public/javascripts/phoneBook.js
@@ -0,0 +1,65 @@
+function post(url, data) {
+    return $.post({
+        url: url,
+        data: JSON.stringify(data),
+        contentType: "application/JSON"
+    });
+}
+
+function get(url, data) {
+    return $.get(url, data);
+}
+
+new Vue({
+    el: "#app",
+    data: {
+        contacts: [],
+        name: "",
+        surname: "",
+        phone: "",
+        term: ""
+    }, created: function () {
+        this.loadData();
+    }, methods: {
+        addContact: function () {
+            if (this.name === "" || this.surname === "" || this.phone === "") {
+                return alert("Вы ничего не ввели!");
+            }
+            var self = this;
+            var data = {
+                name: this.name,
+                surname: this.surname,
+                phone: this.phone
+            };
+            post("/addContact", data).done(function (response) {
+                if (!response.success) {
+                    return alert(response.message);
+                }
+                self.name = "";
+                self.surname = "";
+                self.phone = "";
+                self.loadData();
+
+            });
+        }, deleteContact: function (c) {
+            var self = this;
+            var data = {id: c.id};
+            post("/deleteContact", data).done(function (response) {
+                if (!response.success) {
+                    return alert(response.message);
+                }
+                self.loadData();
+            });
+        }, loadData: function () {
+            var self = this;
+            var data = {term: this.term};
+            get("/getContacts", data).done(function (contacts) {
+                self.contacts = contacts;
+            });
+        }, search: function () {
+            var self = this;
+            this.loadData();
+            self.term = "";
+        }
+    }
+});
